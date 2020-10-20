4461cd41714e9a434958372675bbf0f6b38c0c21
Florian Hansmann <hansmann.f@gmail.com>
2020-10-20 21:02:14 +0200

    u

--- pages/api/potholes/clear.ts
+++ pages/api/potholes/clear.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+/* import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
@@ -15,3 +15,4 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     await prisma.disconnect();
   }
 }
+ */
