293aa7e6b96ea8f68fc45da8791b2364d2f32d83
Florian Hansmann <hansmann.f@gmail.com>
2020-10-21 17:40:37 +0200

    next-seo-v2

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import PropTypes from 'prop-types';
 import {
   Button,
   Flex,
--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,14 @@ import GlobalStyle from '../styles/GlobalStyle';
 import '@reach/combobox/styles.css';
 import '../styles/search.css';
 import '../styles/background.css';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
+        <DefaultSeo {...SEO} />
         <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
